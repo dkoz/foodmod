d085544fb13a8ca69731054cb11b193cfdb9398e
KoZ <mybbkoz@gmail.com>
2013-07-20 09:45:02 -0400

    fixed egg problem

--- lua/entities/food_egg/shared.lua
+++ lua/entities/food_egg/shared.lua
@@ -1,8 +1,8 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.Category = "Egg"
-ENT.PrintName = "Fettuccine Alfredo"
+ENT.Category = "Food Mod"
+ENT.PrintName = "Egg"
 ENT.Author = "KoZ"
 ENT.Purpose = "Eating"
 ENT.Instructions = "A delicious egg cooked sunny side up."
