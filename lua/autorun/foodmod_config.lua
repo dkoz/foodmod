3ab12217d8f06b35d0005c9e5a1aae622cf0359b
KoZ <mybbkoz@gmail.com>
2013-12-31 18:27:05 -0500

    added option to disable sounds

--- lua/autorun/foodmod_config.lua
+++ lua/autorun/foodmod_config.lua
@@ -3,6 +3,9 @@ foodmod = {} -- Do not edit this line
 -- Changes the mode of the food mod ( 1 = health, 2 = hungermod )
 foodmod.mode = 1
 
+-- Disables the default foodmod sounds.
+foodmod.enablesounds = true
+
 -- Mode 1 configuration
 foodmod.healthgain = 15
 
--- lua/entities/food_base/init.lua
+++ lua/entities/food_base/init.lua
@@ -3,13 +3,13 @@ AddCSLuaFile("shared.lua")
 include( "shared.lua" )
 
 function ENT:SpawnFunction( ply, tr, class )
-    if ( !tr.Hit ) then return end
-    local pos = tr.HitPos + tr.HitNormal * 4
-    local ent = ents.Create( class )
-    ent:SetPos( pos )
-    ent:Spawn()
-    ent:Activate()
-    return ent
+	if ( !tr.Hit ) then return end
+	local pos = tr.HitPos + tr.HitNormal * 4
+	local ent = ents.Create( class )
+	ent:SetPos( pos )
+	ent:Spawn()
+	ent:Activate()
+	return ent
 end
 
 function ENT:Initialize()
@@ -35,6 +35,8 @@ function ENT:Use( activator )
 		activator:setSelfDarkRPVar( "Energy", math.Clamp( (energy or 100) + foodmod.energygain, 0, 100 ) )
 	end
 	
-	activator:EmitSound( self.foodSound, 50, 100 )
+	if foodmod.enablesound then
+		activator:EmitSound( self.foodSound, 50, 100 )
+	end
 	self:Remove()
 end
